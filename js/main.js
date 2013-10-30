5804e22a68744b12e1a675f6813ffe0d779c2b51
Jon Dang <jondang@gmail.com>
2013-10-30 19:47:36 -0400

    fixed for multi panel scroll

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 /* custom js */
 
 $('#title').attr( "data-0", "background-color:rgb(113,208,173); top: 0%;" );
-$('#title').attr( "data-500", "background-color:rgb(174,207,110); top: -100%;" );
+$('#title').attr( "data-200", "background-color:rgb(174,207,110); top: -100%;" );
 
+$('.panel:nth-of-type(2)').attr( "data-400", "top: 0%;" );
+$('.panel:nth-of-type(2)').attr( "data-600", "top: -100%;" );
 
-
+$('.panel:nth-of-type(3)').attr( "data-800", "top: 0%;" );
+// $('.panel:nth-of-type(3)').attr( "data-100", "top: -100%;" );
+$('.panel:nth-of-type(3)').children('h1').attr( "data-1000", "color:rgb(57,111,172);" );
+$('.panel:nth-of-type(3)').children('h1').attr( "data-1200", "color:rgb(83,32,96);" );
+$('.panel:nth-of-type(3)').children('h1').attr( "data-1400", "color:rgb(179,230,218);" );
+$('.panel:nth-of-type(3)').children('h1').attr( "data-1600", "color:rgb(180,86,60);" );
 
 skrollr.init();
