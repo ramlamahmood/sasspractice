db17ca2fe8480d79eecb26d96b3fe380b78c5ff9
Jon Dang <jondang@gmail.com>
2013-10-30 18:54:29 -0400

    initial panel setup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
+/* custom js */
+
+$('#title').attr( "data-0", "background-color:rgb(113,208,173); top: 0%;" );
+$('#title').attr( "data-500", "background-color:rgb(174,207,110); top: -100%;" );
+
+
+
+
 skrollr.init();
-
-
-
-
-
-
